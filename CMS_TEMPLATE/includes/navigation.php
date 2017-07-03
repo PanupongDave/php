89d24c5888c44e15a819ca9bca6f08be63b5da8a
PanupongDeve <5635512110@psu.ac.th>
2017-07-03 07:04:32 +0700

    Edin Admin dashboard

--- CMS_TEMPLATE/includes/navigation.php
+++ CMS_TEMPLATE/includes/navigation.php
@@ -15,26 +15,21 @@
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                 <?php
-                $query = "SELECT * FROM categories";
-                $select_all_categories_query = mysqli_query($connection,$query);
+                    $query = "SELECT * FROM categories";
+                    $select_all_categories_query = mysqli_query($connection,$query);
 
-                while($row = mysqli_fetch_assoc($select_all_categories_query)){
-                    $cat_title = $row['cat_title'];
+                    while($row = mysqli_fetch_assoc($select_all_categories_query)){
+                        $cat_title = $row['cat_title'];
 
-                    echo "<li><a href='#'>{$cat_title}</a></li>";
-                }
-
+                        echo "<li><a href='#'>{$cat_title}</a></li>";
+                    }
                 ?>
 
-                 <!--    <li>
-                        <a href="#">About</a>
-                    </li>
-                    <li>
-                        <a href="#">Services</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li> -->
+                <li>
+                    <a href="admin">admin</a>
+                </li>
+
+                
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
--- CMS_TEMPLATE/includes/sidebar.php
+++ CMS_TEMPLATE/includes/sidebar.php
@@ -1,17 +1,11 @@
 <div class="col-md-4">
-<?php 
- if(isset($_POST['submit'])){
-   $search = $_POST['search'];
 
-   $query = "SELECT * FROM posts  WHERE post_tags LINKE '%$search$'";
- }
-?>
 
 
                 <!-- Blog Search Well -->
                 <div class="well">
                     <h4>Blog Search</h4>
-                    <form action="" method="post">
+                    <form action="search.php" method="post">
                     <div class="input-group">
                         <input name="search" type="text" class="form-control">
                         <span class="input-group-btn">
@@ -26,34 +20,23 @@
 
                 <!-- Blog Categories Well -->
                 <div class="well">
-                    <h4>Blog Categories</h4>
+                    <h4>Categories</h4>
                     <div class="row">
                         <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
+                        <?php
+                                $query = "SELECT * FROM categories";
+                                $select_all_categories_sidebar = mysqli_query($connection,$query);
+
+                                while($row = mysqli_fetch_assoc($select_all_categories_sidebar)){
+                                    $cat_title = $row['cat_title'];
+                                    echo "<ul>";
+                                    echo "<li><a href='#'>{$cat_title}</a></li>";
+                                    echo "</ul>";
+                                }
+                ?>
                         </div>
                         <!-- /.col-lg-6 -->
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.col-lg-6 -->
+                 
                     </div>
                     <!-- /.row -->
                 </div>
